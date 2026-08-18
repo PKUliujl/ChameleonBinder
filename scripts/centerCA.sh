8065ec0f355b47f958a7d2441603b0f7f4dbb75d
PKUliujl <110894511+PKUliujl@users.noreply.github.com>
2026-08-18 10:27:01 +0800

    Update centerCA.sh

--- scripts/centerCA.sh
+++ scripts/centerCA.sh
@@ -1,8 +1,8 @@
 grep '^ATOM' $1 | grep ' CA ' | awk '
   {
-    xsum += substr($0, 31, 8) + 0  # extract coordinate of X （column 31-38）
-    ysum += substr($0, 39, 8) + 0  # extract coordinate of Y（clumn 39-46）
-    zsum += substr($0, 47, 8) + 0  # extract coordinate of Z（column 47-54）
+    xsum += substr($0, 31, 8) + 0  # extract coordinate of X (column 31-38)
+    ysum += substr($0, 39, 8) + 0  # extract coordinate of Y (clumn 39-46)
+    zsum += substr($0, 47, 8) + 0  # extract coordinate of Z (column 47-54)
     cnt++
   }
   END {
