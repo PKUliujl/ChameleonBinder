29be03affa459f10678334ebb6f5da4674ab6c61
PKUliujl <110894511+PKUliujl@users.noreply.github.com>
2026-08-18 10:26:18 +0800

    Update centerCA.sh

--- scripts/centerCA.sh
+++ scripts/centerCA.sh
@@ -1,8 +1,8 @@
 grep '^ATOM' $1 | grep ' CA ' | awk '
   {
-    xsum += substr($0, 31, 8) + 0  # 提取 X 坐标（第31-38列）
-    ysum += substr($0, 39, 8) + 0  # 提取 Y 坐标（第39-46列）
-    zsum += substr($0, 47, 8) + 0  # 提取 Z 坐标（第47-54列）
+    xsum += substr($0, 31, 8) + 0  # extract coordinate of X （column 31-38）
+    ysum += substr($0, 39, 8) + 0  # extract coordinate of Y（clumn 39-46）
+    zsum += substr($0, 47, 8) + 0  # extract coordinate of Z（column 47-54）
     cnt++
   }
   END {
